290e1c81d368a70da8be5dd0e25913a81f651594
Podda Modz <ravidusaranga04@gmail.com>
2025-02-10 02:48:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "Golden-Queen-MD-t0R3WJDT#YVWyfQxAgEESdLMsBXBKjb_uBQFhdbHYXszBCF1w1mAftrc", 
 
 //  Mongodp URl එක දාන්න 👇👇👇 (උඩු කොමා දෙක ඇතුලේ )
-  MONGODB: process.env.MONGODB || "mongodb+srv://Tttttt:99999978666@cluster0.0ya0s.mongodb.net/",
+  MONGODB: process.env.MONGODB || "mongodb+srv://momiwew875:w0MAeMOYa7DFismQ@cluster0.rlqyb.mongodb.net/",
 
 
 MAX_SIZE: 500,
