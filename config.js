9986c5221f902b790ffc29e24c352506cf27076e
Podda Modz <ravidusaranga04@gmail.com>
2025-02-09 12:45:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -37,10 +37,10 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
   //  සෙසන් අයිඩී එක දාන්න👇👇👇 (උඩු කොමා දෙක ඇතුලේ)
-  SESSION_ID: process.env.SESSION_ID || "", 
+  SESSION_ID: process.env.SESSION_ID || "Golden-Queen-MD-t0R3WJDT#YVWyfQxAgEESdLMsBXBKjb_uBQFhdbHYXszBCF1w1mAftrc", 
 
 //  Mongodp URl එක දාන්න 👇👇👇 (උඩු කොමා දෙක ඇතුලේ )
-  MONGODB: process.env.MONGODB || "",
+  MONGODB: process.env.MONGODB || "mongodb+srv://vefobi7950:D6dwu7gweT3aIsdO@cluster0.vwaqu.mongodb.net/",
 
 
 MAX_SIZE: 500,
